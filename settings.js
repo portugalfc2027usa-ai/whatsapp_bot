9580be27316310b76c7871eaacf58146dea1221a
portugalfc2027usa-ai <portugalfc2027usa@gmail.com>
2026-02-13 02:45:53 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Kenny Bot',
-  author: 'Wambugu Kinyua',
-  botName: "Kinyua Bot",
-  botOwner: 'Wambugu Kinyua', // Your name
-  ownerNumber: '2547xxxxxx', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'PHOXNET',
+  author: 'sigma boy',
+  botName: "LINUX <cdd by PHOX>",
+  botOwner: 'sigma boy PHOX', // Your name
+  ownerNumber: '256747065753', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.0",
 };
